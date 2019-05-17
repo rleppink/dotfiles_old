44437509aa343708fa9ff4cefe33559190c23007
Ronald Leppink <ronaldleppink@gmail.com>
2019-05-17 11:35:37 +0200

    Remove unused dots

---
+++ etc/bonsai.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+clear
+echo ""
+echo ""
+echo -e "              \e[32m&&"
+echo -e "             \e[32m&&&&&"
+echo -e "           \e[32m&&&\/& &&&"
+echo -e "          \e[32m&&\e[33m|,/  |/\e[32m& &&"
+echo -e "           \e[32m&&\e[33m/   /  /_\e[32m&  &&"
+echo -e "             \e[33m\  {  |_____/_\e[32m&"
+echo -e "             \e[33m{  / /          \e[32m&&&"
+echo -e "             \e[33m.\`. \\{___\________\/_\}"
+echo -e "              \e[33m\} \}\{       \\"
+echo -e "              \e[33m}\{\{         \\____\e[32m&"
+echo -e "             \e[33m\{\}\{           \`\e[32m&\e[33m\\e[32m&&"
+echo -e "             \e[33m{{}             \e[32m&&"
+echo -e "       \e[33m, -=-~{ .-^- _"
+echo -e "             \`}"
+echo -e "              {"
+echo -e "                   \e[30mBe good to them, \e[32malways.\e[30m"
+echo
+echo
+
+cal -3
+
+echo
